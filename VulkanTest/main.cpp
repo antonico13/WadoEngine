b2e3d3081ae56eb5eca6e1a8932a2ac4ab46cff2
Antonio Codrea <antonio.codrea20@imperial.ac.uk>
2023-11-06 09:38:52 +0000

    Add Vulkan instance creation

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -19,6 +19,37 @@ public:
 
 private:
     GLFWwindow* window;
+    VkInstance instance;
+
+    void createInstance() {
+        // Need to make this library agnostic with a windowing library translator
+        // eg, in the future get this running on closed platforms as well. 
+        // decouple even API from windowing?
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create Vulkan instance");
+        };
+
+    }
 
     void initWindow() {
         glfwInit();
@@ -27,7 +58,7 @@ private:
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
     void initVulkan() {
-
+        createInstance();
     }   
 
     void mainLoop() {
