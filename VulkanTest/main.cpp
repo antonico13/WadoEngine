0149dd496749fff741ae74a41e71e306837e23b9
Antonio Codrea <antonio.codrea20@imperial.ac.uk>
2023-11-14 13:41:00 +0000

    Add swap surface format choice function

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -419,6 +419,17 @@ private:
         std::vector<VkPresentModeKHR> presentModes;
     };
 
+    VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
+        for (const VkSurfaceFormatKHR& availableFormat : availableFormats) {
+            if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB &&
+                availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+                    return availableFormat;
+                }
+        }
+
+        return availableFormats[0];
+    }
+
     SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device) {
         SwapChainSupportDetails details;
 
