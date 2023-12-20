85dd18cb35cb91645d9e0b026afdd681db73d640
Antonio Codrea <antonio.codrea20@imperial.ac.uk>
2023-12-20 02:21:05 +0200

    Add texture image staging buffer

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -417,6 +417,20 @@ private:
         if (!pixels) {
             throw std::runtime_error("Failed to load texture image!");
         }
+
+        VkBuffer stagingBuffer;
+        VkDeviceMemory stagingBufferMemory;
+
+        createBuffer(imageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_SHARING_MODE_EXCLUSIVE, 
+            VK_MEMORY_PROPERTY_HOST_COHERENT_BIT | VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT, 
+            stagingBuffer, stagingBufferMemory);
+        
+        void* data;
+        vkMapMemory(device, stagingBufferMemory, 0, imageSize, 0, &data);
+        memcpy(data, pixels, static_cast<size_t>(imageSize));
+        vkUnmapMemory(device, stagingBufferMemory);
+
+        stbi_image_free(pixels);
     }
 
     void createDescriptorSets() {
