beaab6e1b24f8e4d4a4a80c7f3fbba831eef14d2
ToniCodrea <50115076+ToniCodrea@users.noreply.github.com>
2024-02-21 17:12:16 +0000

    Add shader compilation script

---
+++ compileShaders.ps1
@@ -0,0 +1,4 @@
+glslc .\Shaders\Fragment\PBRDeferredSecond.frag -o .\Shaders\SPIR-V\PBRDeferredSecondFrag.spv
+glslc .\Shaders\Fragment\PBRDeferredFirst.frag -o .\Shaders\SPIR-V\PBRDeferredFirstFrag.spv
+glslc .\Shaders\Vertex\PBRDeferredFirst.vert -o .\Shaders\SPIR-V\PBRDeferredFirstVert.spv 
+glslc .\Shaders\Vertex\PBRDeferredSecond.vert -o .\Shaders\SPIR-V\PBRDeferredSecondVert.spv 
