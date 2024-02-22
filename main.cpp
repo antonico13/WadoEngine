17e11ed231c66af43f531080e3723a27a8ae0efc
ToniCodrea <50115076+ToniCodrea@users.noreply.github.com>
2024-02-22 14:41:57 +0000

    Add texture index to Vertex equality function

--- main.cpp
+++ main.cpp
@@ -115,7 +115,7 @@ struct Vertex {
     };
 
     bool operator==(const Vertex& other) const {
-        return pos == other.pos && color == other.color && texCoord == other.texCoord && normal == other.normal;
+        return pos == other.pos && color == other.color && texCoord == other.texCoord && normal == other.normal && texIndex == other.texIndex;
     }
 };
 
