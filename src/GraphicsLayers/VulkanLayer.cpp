36815b4a375fc07afd6927a447a39e2aac1ff37d
ToniCodrea <50115076+ToniCodrea@users.noreply.github.com>
2024-04-07 19:00:56 +0100

    Add containers for resource pointers in Vulkan Layer in order to perform garbage collection

--- src/GraphicsLayers/VulkanLayer.cpp
+++ src/GraphicsLayers/VulkanLayer.cpp
@@ -2,5 +2,17 @@
 #include "VulkanLayer.h"
 
 namespace Wado::GAL::Vulkan {
-    
+
+    // containers for garbage collection
+    VulkanLayer::liveImages;
+    VulkanLayer::liveBuffers;
+    VulkanLayer::liveFences;
+    VulkanLayer::liveSemaphores;
+    VulkanLayer::liveCommandPools;
+    VulkanLayer::livePipelines;
+    VulkanLayer::liveRenderPasses;
+
+    WdImage VulkanLayer::create2DImage(WdExtent2D extent, uint32_t mipLevels, WdSampleCount sampleCount, WdFormat imageFormat, WdImageUsageFlags usageFlags) {
+
+    }
 }
--- src/GraphicsLayers/VulkanLayer.h
+++ src/GraphicsLayers/VulkanLayer.h
@@ -1,16 +1,36 @@
 #ifndef H_WD_VULKAN_LAYER
 #define H_WD_VULKAN_LAYER
 
+#define GLFW_INCLUDE_VULKAN
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_ENABLE_EXPERIMENTAL
+#include <GLFW/glfw3.h>
+
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/hash.hpp>
+
 #include "GraphicsAbstractionLayer.h"
 #include <memory>
 
 namespace Wado::GAL::Vulkan {
     class VulkanLayer : public GraphicsLayer {
         public:
+            WdImage VulkanLayer::create2DImage(WdExtent2D extent, uint32_t mipLevels, WdSampleCount sampleCount, WdFormat imageFormat, WdImageUsageFlags usageFlags) override;
             static std::shared_ptr<VulkanLayer> getVulkanLayer();
         private:
             VulkanLayer();
             static std::shared_ptr<VulkanLayer> layer;
+            // the pointer management here will need to change 
+            static std::vector<WdImage*> liveImages;
+            static std::vector<WdBuffer*> liveBuffers;
+            static std::vector<WdFenceHandle> liveFences;
+            static std::vector<VkSemaphore> liveSemaphores;
+            static std::vector<VkCommandPool> liveCommandPools;
+            static std::vector<VkPipeline> livePipelines;
+            static std::vector<VkRenderPass> liveRenderPasses;
             
     };
 }
