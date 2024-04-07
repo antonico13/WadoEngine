24a904c060c84fd0e6d8f536e5ae4ffbd201fd20
ToniCodrea <50115076+ToniCodrea@users.noreply.github.com>
2024-04-07 19:29:04 +0100

    Add sampling global state to Vulkan Layer

--- src/GraphicsLayers/VulkanLayer.cpp
+++ src/GraphicsLayers/VulkanLayer.cpp
@@ -6,11 +6,17 @@ namespace Wado::GAL::Vulkan {
     // containers for garbage collection
     VulkanLayer::liveImages;
     VulkanLayer::liveBuffers;
+    VulkanLayer::liveSamplers;
     VulkanLayer::liveFences;
     VulkanLayer::liveSemaphores;
     VulkanLayer::liveCommandPools;
     VulkanLayer::livePipelines;
     VulkanLayer::liveRenderPasses;
+
+    // global sampling/texturing state 
+    VulkanLayer::enableAnisotropy;
+    VulkanLayer::maxAnisotropy;
+    VulkanLayer::maxMipLevels;
 
     WdImage VulkanLayer::create2DImage(WdExtent2D extent, uint32_t mipLevels, WdSampleCount sampleCount, WdFormat imageFormat, WdImageUsageFlags usageFlags) {
 
--- src/GraphicsLayers/VulkanLayer.h
+++ src/GraphicsLayers/VulkanLayer.h
@@ -23,14 +23,23 @@ namespace Wado::GAL::Vulkan {
         private:
             VulkanLayer();
             static std::shared_ptr<VulkanLayer> layer;
+
+            // used for global sampler and texture creation, based on device
+            // properties and re-calculated every time device is set up.
+            static bool enableAnisotropy;
+            static float maxAnisotropy;
+            static uint32_t maxMipLevels;
+
             // the pointer management here will need to change 
             static std::vector<WdImage*> liveImages;
             static std::vector<WdBuffer*> liveBuffers;
-            static std::vector<WdFenceHandle> liveFences;
+            static std::vector<VkSampler> liveSamplers;
+            static std::vector<VkFence> liveFences;
             static std::vector<VkSemaphore> liveSemaphores;
             static std::vector<VkCommandPool> liveCommandPools;
             static std::vector<VkPipeline> livePipelines;
             static std::vector<VkRenderPass> liveRenderPasses;
+            
             
     };
 }
