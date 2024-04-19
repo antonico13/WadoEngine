f24913273f79f551fe725e29619145fade1e2d0c
ToniCodrea <50115076+ToniCodrea@users.noreply.github.com>
2024-04-19 19:59:10 +0100

    Add WdRenderPass header file and change GraphicsLayer to WdLayer

---
+++ src/GraphicsAbstractionLayer/WdLayer.h
@@ -0,0 +1,64 @@
+#ifndef WADO_GRAPHICS_ABSTRACTION_LAYER
+#define WADO_GRAPHICS_ABSTRACTION_LAYER
+
+#include "WdCommon.h"
+#include "WdImage.h"
+#include "WdBuffer.h"
+#include "WdPipeline.h"
+#include "WdCommandList.h"
+
+#include "Shader.h"
+
+#include "MainClonePtr.h"
+
+#include <vector>
+
+namespace Wado::GAL {
+    // Graphics abstraction layer.
+    // Backends such as Vulkan or Direct X implement this interface,
+    // and all rendering code should be written using these functions
+    class WdLayer {
+        public:
+            // Returns pointer to a WdImage that represents the screen, can only be used as a fragment output!
+            virtual Memory::WdClonePtr<WdImage> getDisplayTarget() = 0;
+            
+            virtual Memory::WdClonePtr<WdImage> create2DImage(WdExtent2D extent, uint32_t mipLevels, 
+                    WdSampleCount sampleCount, WdFormat imageFormat, WdImageUsageFlags usageFlags) = 0;
+
+            virtual Memory::WdClonePtr<WdBuffer> createBuffer(WdSize size, WdBufferUsageFlags usageFlags) = 0;
+
+            // create texture sampler 
+            virtual WdSamplerHandle createSampler(const WdTextureAddressMode& addressMode = DEFAULT_TEXTURE_ADDRESS_MODE, WdFilterMode minFilter = WdFilterMode::WD_LINEAR, WdFilterMode magFilter = WdFilterMode::WD_LINEAR, WdFilterMode mipMapFilter = WdFilterMode::WD_LINEAR) = 0;
+
+            virtual void updateBuffer(const WdBuffer& buffer, void *data, WdSize offset, WdSize dataSize) = 0;
+            // close or open "pipe" between CPU and GPU for this buffer's memory
+            virtual void openBuffer(WdBuffer& buffer) = 0;
+
+            virtual void closeBuffer(WdBuffer& buffer) = 0;
+
+            // immediate versions 
+            virtual void copyBufferToImage(const WdBuffer& buffer, const WdImage& image, WdExtent2D extent) = 0;
+            virtual void copyBuffer(const WdBuffer& srcBuffer, const WdBuffer& dstBuffer, WdSize size) = 0;
+
+            virtual WdFenceHandle createFence(bool signaled = true) = 0;
+
+            virtual void waitForFences(const std::vector<WdFenceHandle>& fences, bool waitAll = true, uint64_t timeout = UINT64_MAX) = 0;
+
+            virtual void resetFences(const std::vector<WdFenceHandle>& fences) = 0;
+
+            virtual Memory::WdClonePtr<WdPipeline> createPipeline(const Shader::WdShader& vertexShader, const Shader::WdShader& fragmentShader, const WdViewportProperties& viewportProperties) = 0;
+
+            virtual Memory::WdClonePtr<WdRenderPass> createRenderPass(const std::vector<WdPipeline>& pipelines) = 0;
+
+            virtual void executeCommandList(const WdCommandList& commandList) = 0;
+
+            virtual Memory::WdClonePtr<WdCommandList> createCommandList() = 0;
+
+            virtual WdFormat findSupportedHardwareFormat(const std::vector<WdFormat>& formatCandidates, WdImageTiling tiling, WdFormatFeatureFlags features) = 0;
+
+            virtual void transitionImageUsage(const WdImage& image, WdImageUsage currentUsage, WdImageUsage nextUsage) = 0;
+
+            virtual void displayCurrentTarget() = 0;
+    };   
+}
+#endif
---
+++ src/GraphicsAbstractionLayer/WdRenderPass.h
@@ -0,0 +1,22 @@
+#ifndef WADO_GRAPHICS_RENDER_PASS
+#define WADO_GRAPHICS_RENDER_PASS
+
+#include <vector>
+
+namespace Wado::GAL {
+    // Forward declaration
+    class WdPipeline;
+
+    class WdRenderPass {
+        public:
+            friend class WdLayer;
+            //friend class Vulkan::VulkanLayer;
+
+        private:
+            WdRenderPass(const std::vector<WdPipeline>& pipelines);
+            const std::vector<WdPipeline>& _pipelines;
+    };
+
+}
+
+#endif
