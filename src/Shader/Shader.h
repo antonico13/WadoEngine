a4590776d3e96e3257a4779ce4f5cae84010eae1
ToniCodrea <50115076+ToniCodrea@users.noreply.github.com>
2024-04-19 19:20:06 +0100

    Change Shader class interface

--- src/Shader/Shader.h
+++ src/Shader/Shader.h
@@ -1,37 +1,17 @@
-#ifndef H_WD_SHADER
-#define H_WD_SHADER
+#ifndef WADO_SHADER
+#define WADO_SHADER
 
-#include "GraphicsAbstractionLayer.h"
 #include <cstdint>
 #include <vector>
 
 namespace Wado::Shader {
 
-using ShaderByteCode = std::vector<uint8_t>;
+    using WdShaderByteCode = const std::vector<uint8_t>&;
 
-class Shader {
-    public:
-        virtual std::vector<ShaderParameter> getShaderUniforms() = 0;
-    private:
-        ShaderByteCode shaderByteCode;
-        std::vector<ShaderParameter> uniforms;
-        std::vector<ShaderParameter> inputs;
-        std::vector<ShaderParameter> outputs;
-};
-
-class VertexShader : public Shader {
-    public:
-        virtual std::vector<ShaderParameter> getShaderUniforms() = 0;
-    private:
-        ShaderByteCode shaderByteCode;
-        std::vector<ShaderParameter> uniforms;
-        std::vector<ShaderParameter> inputs;
-        std::vector<ShaderParameter> outputs;
-        std::vector<ShaderParameter> subpassInputs; // uniforms under any other API and Vulkan
-        // but they need to be handled separately in Vulkan. Since these don't really 
-        // exist for other APIs I thought it was fine to specialize it instead of 
-        // removing it 
-}
-
+    class WdShader {
+        public:
+        private:
+            WdShaderByteCode shaderByteCode;
+    };
 }
 #endif
