ac8cd79f9ac954dbb73a9f1645d6ddd0da9557f9
ToniCodrea <50115076+ToniCodrea@users.noreply.github.com>
2024-04-23 21:41:24 +0100

    Add temp resource creation functionality to Wd Layer

--- src/GraphicsAbstractionLayer/WdLayer.cpp
+++ src/GraphicsAbstractionLayer/WdLayer.cpp
@@ -2,7 +2,8 @@
 
 namespace Wado::GAL {
 
-    uint32_t WdLayer::globalResourceID = 0;
+    WdResourceID WdLayer::globalResourceID = 0;
+    WdResourceID WdLayer::globalTempResourceID = TEMP_RESOURCE_BARRIER;
 
     WdImage* WdLayer::create2DImagePtr(const std::vector<WdImageHandle>& _handles, const std::vector<WdMemoryHandle>& _memories, const std::vector<WdRenderTarget>& _targets, WdFormat _format, WdExtent2D _extent, WdImageUsageFlags _usage, WdClearValue _clearValue) {
         globalResourceID++;
@@ -13,4 +14,17 @@ namespace Wado::GAL {
         globalResourceID++;
         return new WdBuffer(globalResourceID, _handles, _memories, _size, _usage);
     };
+
+    WdImage* WdLayer::createTemp2DImagePtr() {
+        globalTempResourceID++;
+        WdClearValue tempClearValue;
+        tempClearValue.color = DEFAULT_COLOR_CLEAR;
+        return new WdImage(globalTempResourceID, {}, {}, {}, WdFormat::WD_FORMAT_UNDEFINED, {0, 0}, 0, tempClearValue);
+    };
+    
+    WdBuffer* WdLayer::createTempBufferPtr() {
+        globalTempResourceID++;
+        return new WdBuffer(globalTempResourceID, {}, {}, 0, 0);
+    };
+            
 };
--- src/GraphicsAbstractionLayer/WdLayer.h
+++ src/GraphicsAbstractionLayer/WdLayer.h
@@ -22,6 +22,8 @@ namespace Wado::GAL {
             // Utils as static consts here for now 
             static const uint32_t FRAMES_IN_FLIGHT = 3;
             static const int CURRENT_FRAME_RESOURCE = -1;
+            static const uint32_t TEMP_RESOURCE_BARRIER = 0x3FFFFFFF;
+
             // Returns pointer to a WdImage that represents the screen, can only be used as a fragment output!
             virtual Memory::WdClonePtr<WdImage> getDisplayTarget() = 0;
             
@@ -29,6 +31,11 @@ namespace Wado::GAL {
                     WdSampleCount sampleCount, WdFormat imageFormat, WdImageUsageFlags usageFlags, bool multiFrame = false) = 0;
 
             virtual Memory::WdClonePtr<WdBuffer> createBuffer(WdSize size, WdBufferUsageFlags usageFlags, bool multiFrame = false) = 0;
+            // Temporary resources, these are *only* used for pipeline synchronisation creation, the pipeline
+            // parameters that have been set to these objects need to be set to concrete objects before rendering.
+            virtual Memory::WdClonePtr<WdImage> createTemp2DImage(bool multiFrame = false) = 0;
+
+            virtual Memory::WdClonePtr<WdBuffer> createTempBuffer(bool multiFrame = false) = 0;
 
             // create texture sampler 
             virtual WdSamplerHandle createSampler(const WdTextureAddressMode& addressMode = DEFAULT_TEXTURE_ADDRESS_MODE, WdFilterMode minFilter = WdFilterMode::WD_LINEAR, WdFilterMode magFilter = WdFilterMode::WD_LINEAR, WdFilterMode mipMapFilter = WdFilterMode::WD_LINEAR) = 0;
@@ -66,7 +73,11 @@ namespace Wado::GAL {
         protected:
             static WdImage* create2DImagePtr(const std::vector<WdImageHandle>& _handles, const std::vector<WdMemoryHandle>& _memories, const std::vector<WdRenderTarget>& _targets, WdFormat _format, WdExtent2D _extent, WdImageUsageFlags _usage, WdClearValue _clearValue);
             static WdBuffer* createBufferPtr(const std::vector<WdBufferHandle>& _handles, const std::vector<WdMemoryHandle>& _memories, WdSize _size, WdBufferUsageFlags _usage);
-            static uint32_t globalResourceID;
+            static WdImage* createTemp2DImagePtr();
+            static WdBuffer* createTempBufferPtr();
+            
+            static WdResourceID globalResourceID;
+            static WdResourceID globalTempResourceID;
     };   
 }
 #endif
