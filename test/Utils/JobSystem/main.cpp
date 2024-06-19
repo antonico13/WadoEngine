836ccf4ee95b69ec71cedd65f1d85ed16a2d2a7b
ToniCodrea <50115076+ToniCodrea@users.noreply.github.com>
2024-06-19 06:09:04 +0100

    Add benchmarking tools

--- test/Utils/JobSystem/main.cpp
+++ test/Utils/JobSystem/main.cpp
@@ -1,8 +1,13 @@
+#include <benchmark/benchmark.h>
+
+#include <thread>
+
 #include "FiberSystem.h"
 #include "TaskSystem.h"
 
 #include <cstdlib>
 #include <iostream>
+#include <vector>
 
 Wado::FiberSystem::WdLock globalLock;
 
@@ -24,8 +29,8 @@ typedef struct NextData {
 } NextData;
 
 TASK(NextTask, NextData, data, {
-    size_t startCore = (size_t) Wado::Thread::WdThreadLocalGetValue(TLcoreIndexID);
-    coreStart[startCore]++;
+    // size_t startCore = (size_t) Wado::Thread::WdThreadLocalGetValue(TLcoreIndexID);
+    // coreStart[startCore]++;
     Wado::Atomics::Decrement(&count);
     // globalLock.acquire();
     // size_t endCore = (size_t) Wado::Thread::WdThreadLocalGetValue(TLcoreIndexID);
@@ -39,8 +44,8 @@ TASK(NextTask, NextData, data, {
 });
 
 TASK(DummyTask, DummyData, data, {
-    size_t startCore = (size_t) Wado::Thread::WdThreadLocalGetValue(TLcoreIndexID);
-    coreStart[startCore]++;
+    // size_t startCore = (size_t) Wado::Thread::WdThreadLocalGetValue(TLcoreIndexID);
+    // coreStart[startCore]++;
     Wado::Atomics::Decrement(&count);
     // globalLock.acquire();
     // size_t endCore = (size_t) Wado::Thread::WdThreadLocalGetValue(TLcoreIndexID);
@@ -54,55 +59,110 @@ TASK(DummyTask, DummyData, data, {
     Wado::Task::makeTask(NextTask, nullptr, data->nextFence);
 });
 
+static void BM_fiber(benchmark::State& state) {
+
+    for (auto _ : state) {
+
+        Wado::FiberSystem::WdFence *fence = static_cast<Wado::FiberSystem::WdFence *>(malloc(sizeof(Wado::FiberSystem::WdFence)));
+        Wado::FiberSystem::WdFence *nextFence = static_cast<Wado::FiberSystem::WdFence *>(malloc(sizeof(Wado::FiberSystem::WdFence)));
+
+        if ( (fence == nullptr) || (nextFence == nullptr) ) {
+            std::cout << "Could not allocate fence" << std::endl;
+        };
+
+        size_t taskCount = 800;
+        count = 2 * taskCount;
+
+
+        new (fence) Wado::FiberSystem::WdFence(taskCount);
+        new (nextFence) Wado::FiberSystem::WdFence(taskCount);
+
+        //std::cout << "Made fences" << std::endl;
+
+        DummyData data;
+        data.x = 0;
+        data.y = 1;
+        data.nextFence = nextFence;
+
+        for (size_t i = 0; i < taskCount; i++) {
+            Wado::Task::makeTask(DummyTask, &data, fence);
+        };
+
+        //std::cout << "Made tasks" << std::endl;
+
+        fence->waitForSignal();
+
+        nextFence->waitForSignal();
+        
+        //std::cout << "Finished waiting" << std::endl;
+
+        fence->~WdFence();
+        nextFence->~WdFence();
+
+        free(fence);
+        free(nextFence);
+
+        // for (int i = 0; i < 8; i++) {
+        //     std::cout << "Core: " << i << " started " << coreStart[i] << " fibers " << std::endl;
+        //     std::cout << "Core: " << i << " unlocked " << coreEnd[i] << " fibers " << std::endl;
+        // };
+        // std::cout << "Total fibers: " << fiberCount << std::endl;
+
+        // std::cout << "Shutting down " << std::endl;
+        
+        // std::cout << "Count value: " << count << std::endl; 
+    };
+};
+
+static void BM_thread(benchmark::State& state) {
+    auto f = []() {
+        Wado::Atomics::Decrement(&count);
+
+        auto secondTask = []() {
+            Wado::Atomics::Decrement(&count);
+
+        };
+        std::thread innerTask(secondTask);
+
+        innerTask.join();
+    };
+
+    for (auto _ : state) {
+
+        size_t taskCount = state.range(0);
+        count = 2 * taskCount;
+
+        //std::cout << "Made fences" << std::endl;
+        std::vector<std::thread> threads;
+        threads.reserve(taskCount);
+
+
+        for (size_t i = 0; i < taskCount; i++) {
+            threads.emplace_back(f);
+        };
+
+        for (size_t i = 0; i < taskCount; i++) {
+            threads.at(i).join();
+        };
+
+        //std::cout << "Made tasks" << std::endl;
+
+        //fence->waitForSignal();
+
+        //nextFence->waitForSignal();
+        
+        //std::cout << "Finished waiting" << std::endl;
+    };
+};
+
+BENCHMARK(BM_fiber);//->Arg(20)->Arg(40)->Arg(80)->Arg(160)->Arg(320)->Arg(640)->Arg(800);
+
+//BENCHMARK(BM_thread)->Arg(20)->Arg(40)->Arg(80)->Arg(160)->Arg(320)->Arg(640)->Arg(800);
+
+
 int main(int argc, char** argv) {
     Wado::FiberSystem::InitFiberSystem();
-
-    Wado::FiberSystem::WdFence *fence = static_cast<Wado::FiberSystem::WdFence *>(malloc(sizeof(Wado::FiberSystem::WdFence)));
-    Wado::FiberSystem::WdFence *nextFence = static_cast<Wado::FiberSystem::WdFence *>(malloc(sizeof(Wado::FiberSystem::WdFence)));
-
-    if ( (fence == nullptr) || (nextFence == nullptr) ) {
-        std::cout << "Could not allocate fence" << std::endl;
-    };
-
-    const size_t taskCount = 800;
-
-    new (fence) Wado::FiberSystem::WdFence(taskCount);
-    new (nextFence) Wado::FiberSystem::WdFence(taskCount);
-
-    std::cout << "Made fences" << std::endl;
-
-    DummyData data;
-    data.x = 0;
-    data.y = 1;
-    data.nextFence = nextFence;
-
-    for (size_t i = 0; i < taskCount; i++) {
-        Wado::Task::makeTask(DummyTask, &data, fence);
-    };
-
-    std::cout << "Made tasks" << std::endl;
-
-    fence->waitForSignal();
-
-    nextFence->waitForSignal();
-    
-    std::cout << "Finished waiting" << std::endl;
-
-    fence->~WdFence();
-    nextFence->~WdFence();
-
-    free(fence);
-    free(nextFence);
-
-    for (int i = 0; i < 8; i++) {
-        std::cout << "Core: " << i << " started " << coreStart[i] << " fibers " << std::endl;
-        std::cout << "Core: " << i << " unlocked " << coreEnd[i] << " fibers " << std::endl;
-    };
-    std::cout << "Total fibers: " << fiberCount << std::endl;
-
-    std::cout << "Shutting down " << std::endl;
-    
-    std::cout << "Count value: " << count << std::endl;
-    
-    Wado::FiberSystem::ShutdownFiberSystem();
-};
+   ::benchmark::Initialize(&argc, argv);
+   ::benchmark::RunSpecifiedBenchmarks();
+    //Wado::FiberSystem::ShutdownFiberSystem();
+}
