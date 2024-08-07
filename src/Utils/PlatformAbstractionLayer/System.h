718edb058dbf9acad6c0e1f2e7432a3b35a10970
ToniCodrea <50115076+ToniCodrea@users.noreply.github.com>
2024-08-07 15:31:40 +0300

    Change core system to retrieve info about core hyperthreading too

--- src/Utils/PlatformAbstractionLayer/System.h
+++ src/Utils/PlatformAbstractionLayer/System.h
@@ -2,11 +2,18 @@
 #define WADO_PAL_SYSTEM_H
 
 #include <cstdint>
+#include <vector>
 
 namespace Wado::System {
-    using WdAvailableCoresMask = uint64_t;
 
-    WdAvailableCoresMask WdGetAvailableSystemCores();
+    using WdCoreInfo = struct WdCoreInfo {
+        WdCoreInfo(const uint64_t number, const uint64_t neighbour) : coreNumber(number), hyperthreadLocalNeighbour(neighbour) { };
+        const uint64_t coreNumber;
+        const uint64_t hyperthreadLocalNeighbour;
+    };
+
+    // This uses ROV so it's fine, vector *SHOULD* fit in the return slot on x86-64 
+    const std::vector<WdCoreInfo> WdGetAvailableSystemCores();
 };
 
 #endif
--- src/Utils/PlatformAbstractionLayer/Windows/System.cpp
+++ src/Utils/PlatformAbstractionLayer/Windows/System.cpp
@@ -3,19 +3,62 @@
 #include "System.h"
 
 #include <stdexcept>
+#include <bitset>
 
 namespace Wado::System {
-    WdAvailableCoresMask WdGetAvailableSystemCores() {
+    // TODO: no support for NUMA or more than 64 cores yet 
+    const std::vector<WdCoreInfo> WdGetAvailableSystemCores() {
+        std::vector<WdCoreInfo> coreInfos;
         HANDLE currentProcess = GetCurrentProcess();
         DWORD_PTR processAffinityMask;
         DWORD_PTR systemAffinityMask;
         BOOL res;
+
         res = GetProcessAffinityMask(currentProcess, &processAffinityMask, &systemAffinityMask);
 
         if (res == FALSE) {
-            throw std::runtime_error("Could not get system information");
+            throw std::runtime_error("Could not get system core information");
         };
 
-        return static_cast<WdAvailableCoresMask>(processAffinityMask);
+        DWORD length;                             
+
+        res = GetLogicalProcessorInformation(NULL, &length);
+
+        PSYSTEM_LOGICAL_PROCESSOR_INFORMATION buffer = static_cast<PSYSTEM_LOGICAL_PROCESSOR_INFORMATION>(HeapAlloc(GetProcessHeap(), 0, sizeof(SYSTEM_LOGICAL_PROCESSOR_INFORMATION)));
+        
+        res = GetLogicalProcessorInformation(buffer, &length);
+
+        if (res == FALSE) {
+            throw std::runtime_error("Could not get logical core information");
+        };
+
+        DWORD seek = 0;
+        length = length / sizeof(SYSTEM_LOGICAL_PROCESSOR_INFORMATION);
+        while (seek < length) {
+            SYSTEM_LOGICAL_PROCESSOR_INFORMATION sysInfo = *(buffer + seek);
+            // TODO: Should I care about cache size and stuff here?
+            if ((sysInfo.Relationship == RelationProcessorCore) && (sysInfo.ProcessorCore.Flags == 1)) {
+                // logical processors mentioned by the processor mask share a physical processor 
+                // need to make sure all cores are usable 
+                if (sysInfo.ProcessorMask & processAffinityMask == sysInfo.ProcessorMask) {
+                    uint64_t firstCore = 0;
+                    uint64_t secondCore = 0;
+                    std::bitset<sizeof(ULONG_PTR)> processorMask = std::bitset<sizeof(ULONG_PTR)>(sysInfo.ProcessorMask);
+                    for (size_t i = 0; i < sizeof(ULONG_PTR); ++i) {
+                        if (processorMask[i]) {
+                            firstCore = secondCore;
+                            secondCore = i;
+                        };
+                    };
+
+                    coreInfos.emplace_back(firstCore, secondCore);
+                    coreInfos.emplace_back(secondCore, firstCore);
+                };
+            };
+            ++seek;
+        };
+
+        // This should make use of RVO given that the return slot can hold a vector 
+        return coreInfos;
     };
 };
